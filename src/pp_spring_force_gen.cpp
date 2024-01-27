2269d011c9773287055332a36b6f934106734fb7
Henrique Antonio Lustosa Ribeiro Silva <henriquealrs@gmail.com>
2024-01-27 16:48:53 -0300

    Spring demo

--- src/pp_spring_force_gen.cpp
+++ src/pp_spring_force_gen.cpp
@@ -6,8 +6,9 @@ using namespace simphys::sim;
 void PPSpringForceGen::UpdateForce(Particle &particle, double duration)
 {
     const auto d_vec = particle.GetPos() - anchor_.GetPos();
-    const double force_mag = d_vec.mag() * k_;
+    const double force_mag = (d_vec.mag() - rest_length_) * k_;
 
     const auto force = (-force_mag) * math::UnitVec3(d_vec);
     particle.ApplyForce(force);
+    anchor_.ApplyForce((-1.)*force);
 }
