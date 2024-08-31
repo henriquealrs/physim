ae9339831d1551b983d10806f5cd6654f7b2961a
Henrique Antonio Lustosa Ribeiro Silva <henriquealrs@gmail.com>
2024-08-31 11:39:44 -0300

    Fix test build and correctness of Spring test

--- unit_tests/math/TestParticle.cpp
+++ unit_tests/math/TestParticle.cpp
@@ -1,6 +1,9 @@
 #include <cmath>
 #include <gtest/gtest.h>
+#include "i_particle_force_gen.hpp"
 #include "objects/particle.hpp"
+#include "gravity_force_gen.hpp"
+#include "fixed_point_spring_force_gen.hpp"
 #include <iostream>
 
 using namespace simphys::math;
@@ -10,7 +13,7 @@ TEST(ParticleTest, Integrator)
     double totalT = 5;
     double dT = 0.001;
 
-    auto particle = simphys::sim::Particle(Vec3(0, 0, 0), Vec3(0, 0, -10), 5);
+    auto particle = simphys::sim::Particle(Vec3(0, 0, 0), Vec3(0, 0, -10), 5, 0.01);
 
     double t = 0;
     while(t < totalT)
@@ -27,28 +30,29 @@ TEST(ParticleTest, Integrator)
 
 TEST(ParticleTest, ParticleHangingOnSpring)
 {
-    double totalT = 3000;
+    double totalT = 30000;
     double dT = 0.001;
     double m = 1.5;
+    double rest_len = 0.5;
     double g = 10;
     double k = 2;
 
-    auto particle = simphys::sim::Particle(Vec3(0, 0, -1),Vec3(0, 0, -g), m);
+    auto particle = simphys::sim::Particle(Vec3(0, 0, -1), Vec3(0,0,0), m, 0.01);
+    auto spring = simphys::sim::FixedPointSpringForceGen(Vec3(0, 0, 0), k, rest_len);
+    auto gravity = simphys::sim::GravityParticleForceGen(Vec3(0, 0, -g));
+    auto registry = simphys::sim::ParticleForceRegistry("Forces");
+    registry.Add(particle, gravity);
+    registry.Add(particle, spring);
 
     double t = 0;
     while(t < totalT)
     {
-        const auto pos = particle.GetPos();
-
-        const auto spring_force = (-k) * particle.GetPos();
-        particle.ApplyForce(spring_force);
-        particle.ApplyForce( (-0.1) * particle.GetVelocity());
-
+        registry.UpdateForces(dT);
         particle.Integrate(dT);
         t += dT;
     }
 
     const auto pos = particle.GetPos();
-    EXPECT_EQ(pos, Vec3(0, 0, - m * g / k));
+    EXPECT_EQ(pos, Vec3(0, 0, -rest_len  - m * g / k));
 }
 
--- unit_tests/sim/test_ParticleContact.cpp
+++ unit_tests/sim/test_ParticleContact.cpp
@@ -11,11 +11,11 @@ TEST(ParticleContact, TestInelasticColision)
     const auto null_vec = Vec3(0, 0, 0);
     const auto  v1 = Vec3(5, 0, 0);
     const double m1 = 5;
-    auto p1 = Particle(null_vec, v1, m1);
+    auto p1 = Particle(null_vec, v1, m1, 0.01);
 
     const auto  v2 = Vec3(0, 0, 0);
     const double m2 = 10;
-    auto p2 = Particle(null_vec, v2, m2);
+    auto p2 = Particle(null_vec, v2, m2, 0.01);
     
     Particle *ps[2] = {&p1, &p2};
     auto collision = ParticleContact(ps, 0, Vec3(-1, 0, 0), 0.01);
@@ -31,11 +31,11 @@ TEST(ParticleContact, TestElasticColision)
     const auto null_vec = Vec3(0, 0, 0);
     const auto  v1 = Vec3(5, 0, 0);
     const double m1 = 5;
-    auto p1 = Particle(null_vec, v1, m1);
+    auto p1 = Particle(null_vec, v1, m1, 0.01);
 
     const auto  v2 = Vec3(0, 0, 0);
     const double m2 = 10;
-    auto p2 = Particle(null_vec, v2, m2);
+    auto p2 = Particle(null_vec, v2, m2, 0.01);
 
     Particle *ps[2] = {&p1, &p2};
     auto collision = ParticleContact{ps, 1, Vec3(-1, 0, 0), 0.01};
