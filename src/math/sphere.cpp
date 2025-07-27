fde7de1dc6a56a88cf4d2aac73336e906d895f29
henriquealrs <henriquealrs@gmail.com>
2025-07-27 12:43:10 -0300

    Implement Sphere collision detection

--- src/math/sphere.cpp
+++ src/math/sphere.cpp
@@ -7,7 +7,15 @@ using simphys::math::Ray;
 
 bool Sphere::Colision(const Geometry &other) const noexcept
 {
-    return false;
+    // try sphere-sphere collision first
+    if(const auto* s = dynamic_cast<const Sphere*>(&other)) {
+        const auto dist_sqr = (this->position_ - s->position_).mag_sqr();
+        const auto radius_sum = this->radius_ + s->radius_;
+        return dist_sqr <= radius_sum * radius_sum;
+    }
+
+    // Fallback to other's implementation for different shapes
+    return other.Colision(*this);
 }
 
 bool Sphere::RayHit(const Ray &ray) const noexcept
